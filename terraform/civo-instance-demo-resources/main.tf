e52a4920c48eb7d158196cb9d6aa697092a8a234
Youcef Guichi <youcef@gimlet.io>
2023-09-21 09:54:26 +0200

    added readable terraform plan

--- terraform/civo-instance-demo-resources/main.tf
+++ terraform/civo-instance-demo-resources/main.tf
@@ -19,7 +19,7 @@ provider "civo" {
 
 resource "civo_instance" "foo" {
     hostname = "tfcontroller.demo"
-    tags = ["hello", "kcd", "austria"]
+    tags = ["hey", "kcd", "austria"]
     notes = "This server was created via tf-controller"
     size = "g3.small"
     disk_image = "debian-10"
