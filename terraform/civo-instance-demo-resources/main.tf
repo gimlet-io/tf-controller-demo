41eb7ea58022bf490f013b19c6b4ec87f00ef4f1
Youcef Guichi <youcef@gimlet.io>
2023-09-24 19:50:06 +0200

    Update tag

--- terraform/civo-instance-demo-resources/main.tf
+++ terraform/civo-instance-demo-resources/main.tf
@@ -19,7 +19,7 @@ provider "civo" {
 
 resource "civo_instance" "foo" {
     hostname = "tfcontroller.demo"
-    tags = ["hey", "kcd", "austria"]
+    tags = ["kcd", "austria"]
     notes = "This server was created via tf-controller"
     size = "g3.small"
     disk_image = "debian-10"
